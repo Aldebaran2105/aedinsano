460eeee0544effdbe175d03a9311e90f333b34e5
stefano565-Utec <142551373+stefano565-Utec@users.noreply.github.com>
2026-02-03 11:22:23 -0500

    Implement insert and remove methods in ThreadedBST

--- ThreadedBST.cpp
+++ ThreadedBST.cpp
@@ -1,5 +1,7 @@
 #include "ThreadedBST.h"
 
+using namespace std;
+
 
 void ThreadedBST::insert(int key) {
     root = insert(root, key);
@@ -10,21 +12,163 @@ void ThreadedBST::remove(int key) {
 }
 
 Node* ThreadedBST::insert(Node* node, int key) {
-    /// COMPLETAR
+    if (node == nullptr) {
+        return new Node(key);
+    }
+
+    Node* parent = nullptr;
+    Node* curr = node;
+
+    while (curr != nullptr) {
+        if (key == curr->key) return node;
+        
+        parent = curr;
+        
+        if (key < curr->key) {
+            if (!curr->lthread)
+                curr = curr->left;
+            else
+                break;
+        } 
+        else {
+            if (!curr->rthread)
+                curr = curr->right;
+            else
+                break;
+        }
+    }
+
+    Node* newNode = new Node(key);
+
+    if (key < parent->key) {
+        
+        newNode->left = parent->left; 
+        newNode->right = parent;      
+        parent->left = newNode;
+        parent->lthread = false;
+    } else {
+        newNode->left = parent;        
+        newNode->right = parent->right;
+        parent->right = newNode;
+        parent->rthread = false;
+    }
+
     return node;
 }
 
 void ThreadedBST::inorder() const {
-    /// COMPLETAR
+    if (root == nullptr) return;
+
+    Node* curr = root;
+
+    while (!curr->lthread)
+        curr = curr->left;
+
+    while (curr != nullptr) {
+        cout << curr->key << " ";
+
+        if (curr->rthread) {
+            curr = curr->right;
+        } else {
+            curr = curr->right;
+            if (curr == nullptr) break;
+            while (!curr->lthread)
+                curr = curr->left;
+        }
+    }
+    cout << endl;
 }
 
 Node* ThreadedBST::findMin(Node* node) const {
-    /// COMPLETAR
+    if (node == nullptr) return nullptr;
+    while (!node->lthread) {
+        node = node->left;
+    }
     return node;
 }
 
 Node* ThreadedBST::remove(Node* node, int key) {
-    /// COMPLETAR
+    Node* parent = nullptr;
+    Node* curr = node;
+    bool found = false;
+
+    while (curr != nullptr) {
+        if (key == curr->key) {
+            found = true;
+            break;
+        }
+        parent = curr;
+        if (key < curr->key) {
+            if (!curr->lthread) curr = curr->left;
+            else break;
+        } else {
+            if (!curr->rthread) curr = curr->right;
+            else break;
+        }
+    }
+
+    if (!found) return node;
+
+    if (!curr->lthread && !curr->rthread) {
+        Node* succ = curr->right;
+        while (!succ->lthread) succ = succ->left;
+        
+        int val = succ->key;
+        remove(node, succ->key); 
+        curr->key = val;
+        return node;
+    }
+
+    Node* child = nullptr;
+    if (!curr->lthread) child = curr->left;
+    else if (!curr->rthread) child = curr->right;
+    else child = nullptr;
+
+    if (child == nullptr) {
+        if (parent == nullptr) {
+            delete curr;
+            return nullptr;
+        }
+        
+        if (curr == parent->left) {
+            parent->lthread = true;
+            parent->left = curr->left;
+        } else {
+            parent->rthread = true;
+            parent->right = curr->right;
+        }
+        delete curr;
+    }
+    else {
+        if (!curr->lthread) {
+             Node* temp = curr->left;
+             while (!temp->rthread) temp = temp->right;
+             temp->right = curr->right;
+             
+             if (parent == nullptr) {
+                 delete curr;
+                 return child;
+             }
+             
+             if (curr == parent->left) parent->left = child;
+             else parent->right = child;
+        } 
+        else {
+             Node* temp = curr->right;
+             while (!temp->lthread) temp = temp->left;
+             temp->left = curr->left;
+
+             if (parent == nullptr) {
+                 delete curr;
+                 return child;
+             }
+
+             if (curr == parent->left) parent->left = child;
+             else parent->right = child;
+        }
+        delete curr;
+    }
+
     return node;
 }
 
